9861cdd17370ef566630c036ad3d33c5448f176c
Stephany K <stephanykleen@gmail.com>
2023-02-16 17:06:20 -0800

    Created a static page using react

---
+++ src/components/MainContent.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function MainContent(){
+  return(
+    <main className="main-content">
+      <h1 className="main-title">Fun facts about React</h1>
+      <ul className="main-list">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  )
+}
+export default MainContent
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function Nav(){
+  return(
+    <div className="nav">
+      <img src="./assets/react.svg" className="logo"/>
+      <h3 className="text-logo">ReactFacts</h3>
+      <h4 className="project-title">React Course - Project 1</h4>
+    </div>
+  )
+}
+export default Nav
